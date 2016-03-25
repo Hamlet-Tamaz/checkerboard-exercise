0dea381fb1042a8fcbe5b5bf4a0a2d5c34020d73
Hamlet Tamazian <hamlettamaz@gmail.com>
2016-03-25 11:15:48 -0700

    done with gradient

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 window.onload = function () {
 
 console.log('sanity check');
+var randN1 = Math.floor(Math.random() *250);
+var randN2 = Math.floor(Math.random() *250);
 var count = 1;
-var c1 = "rgb(randN(), randN(), randN())";
-var c2 = "rgb(randN(), randN(), randN())";
 
 var body = document.getElementsByTagName('body')[0];
 var containerDiv = document.createElement('div');
 
 
-
 body.appendChild(containerDiv);
 
 for (var i = 1; i < 100; i++) {
-	var randN1 = Math.floor(Math.random() *250);
-	var randN2 = Math.floor(Math.random() *250);
-	var randN3 = Math.floor(Math.random() *250);
-	
+	var n1 = randN1;
+	var n2 = randN2;
+
 	var div = document.createElement('div');
 	div.className = "grid";
 	div.style.width = '11.1%';
@@ -31,12 +29,12 @@ for (var i = 1; i < 100; i++) {
 
 	// debugger
 	if (i%2===1) {
-		div.style.backgroundColor = `rgb(${randN1}, ${randN2}, ${randN3})`;
+		div.style.backgroundColor = `rgb(${n1}, ${n1}, ${n1 + (i * 2)})`;
 		containerDiv.appendChild(div);
 		// div.innerText = `${count}`;
 	}
 	else {
-		div.style.backgroundColor = `rgb(${randN1}, ${randN2}, ${randN3})`;
+		div.style.backgroundColor = `rgb(${n2}, ${n2}, ${n2 + (i * 2)})`;
 		containerDiv.appendChild(div);
 		// div.innerText = `${count}`;
 	}	
