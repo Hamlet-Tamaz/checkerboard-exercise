2364565c7da2c925ff92fd888c9fd6302947f3cc
Hamlet Tamazian <hamlettamaz@gmail.com>
2016-03-25 11:07:14 -0700

    done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function () {
 
 console.log('sanity check');
 var count = 1;
+var c1 = "rgb(randN(), randN(), randN())";
+var c2 = "rgb(randN(), randN(), randN())";
 
 var body = document.getElementsByTagName('body')[0];
 var containerDiv = document.createElement('div');
@@ -11,25 +13,31 @@ var containerDiv = document.createElement('div');
 body.appendChild(containerDiv);
 
 for (var i = 1; i < 100; i++) {
+	var randN1 = Math.floor(Math.random() *250);
+	var randN2 = Math.floor(Math.random() *250);
+	var randN3 = Math.floor(Math.random() *250);
+	
 	var div = document.createElement('div');
+	div.className = "grid";
 	div.style.width = '11.1%';
 	div.style.height = '50px';
 	div.style.paddingBoddom = '11.1%';
 	div.style.display = 'inline-block';
 	div.style.color = 'white';
-	// div.innerText = `${count}`;
 	div.style.float = 'left';
+	// div.innerText = `${count}`;
+
 
 
 	// debugger
 	if (i%2===1) {
+		div.style.backgroundColor = `rgb(${randN1}, ${randN2}, ${randN3})`;
 		containerDiv.appendChild(div);
-		div.style.backgroundColor = 'black';
 		// div.innerText = `${count}`;
 	}
 	else {
+		div.style.backgroundColor = `rgb(${randN1}, ${randN2}, ${randN3})`;
 		containerDiv.appendChild(div);
-		div.style.backgroundColor = 'red';
 		// div.innerText = `${count}`;
 	}	
 	count++;
